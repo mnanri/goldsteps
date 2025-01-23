56ead5b05a56c57cd5179457921336f7d3d14d51
mnanri <nanristeps@icloud.com>
2025-01-23 21:32:20 +0900

    user <- event

--- models/user.go
+++ models/user.go
@@ -4,11 +4,11 @@ import "time"
 
 // User Model
 type User struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email" gorm:"unique"`             // Email address is unique
-	Password  string    `json:"-"`                               // Password is private
-	Events    []Event   `json:"events" gorm:"foreignKey:UserID"` // Relationship between
+	ID       uint   `json:"id" gorm:"primaryKey"`
+	Name     string `json:"name"`
+	Email    string `json:"email" gorm:"unique"` // Email address is unique
+	Password string `json:"-"`                   // Password is private
+	// Events    []Event   `json:"events" gorm:"foreignKey:UserID"` // Relationship between
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
