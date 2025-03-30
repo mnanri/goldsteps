d757b13b1557d35fab58050845e625b2e19da26e
mnanri <nanristeps@icloud.com>
2025-03-30 21:00:34 +0900

    fix import pass

--- server/routes/stock_master_routes.go
+++ server/routes/stock_master_routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 	"server/repository"
 
@@ -8,17 +9,19 @@ import (
 )
 
 const stockfile = "stock_master_data/stock_fundamental_202502.csv"
-const stockDetailfile = "stock_/stock_profile_202502.csv"
+const stockDetailfile = "stock_master_data/stock_profile_202502.csv"
 
 func importStockMasterDataFromCSV(c echo.Context) error {
 
 	err := repository.ImportStocks(stockfile)
 	if err != nil {
+		fmt.Println("Error importing stocks:", err)
 		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to save data in Stock"})
 	}
 
 	err = repository.ImportStockDetails(stockDetailfile)
 	if err != nil {
+		fmt.Println("Error importing stock details:", err)
 		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to save data in StockDetail"})
 	}
 
